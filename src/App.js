72c11eb3e488ace4cfef002522cd41c69861ccb2
afdmacdo <afdmacdo@uwaterloo.ca>
2020-05-18 01:05:26 -0400

    basic navigation

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {Component } from 'react';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import './App.css';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
+
+class App extends Component {
+    render () {
+        return (
+            <div className>
+                <Layout fixedHeader>
+                    <Header title={<span style={{ color: '#ddd' }}>Title</span>} scroll>
+                        <Navigation>
+                            <Link to="/resume">Resume</Link>
+                            <Link to="/aboutme">About Me</Link>
+                            <Link to="/projects">Projects</Link>
+                        </Navigation>
+                    </Header>
+                    <Drawer title="Title">
+                        <Navigation>
+                        <Link to="/Resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className="page-content" />
+                        <Main/>
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
+}
+
+export default App;
