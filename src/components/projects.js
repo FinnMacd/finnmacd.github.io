272be402b77ee9dfeedc4614046f8e22ad8f4616
afdmacdo <afdmacdo@uwaterloo.ca>
2020-06-08 20:51:35 -0400

    added poject cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,124 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl';
 
 class Project extends Component {
-    render(){
-        return(
-            <div><h1>ProjectPage</h1></div>
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+
+        switch (this.state.activeTab) {
+            //React
+            case 0:
+                return (
+                    <div className="projects-grid">
+                        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                            <CardTitle style={{
+                                color: 'black', height: '176px', background:
+                                    'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'
+                            }}>
+                                React project 1
+                            </CardTitle>
+                            <CardText>
+                                This is my project, so good!
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+
+                        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                            <CardTitle style={{
+                                color: 'black', height: '176px', background:
+                                    'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'
+                            }}>
+                                React project 1
+                            </CardTitle>
+                            <CardText>
+                                This is my project, so good!
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+
+                        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                            <CardTitle style={{
+                                color: 'black', height: '176px', background:
+                                    'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'
+                            }}>
+                                React project 1
+                            </CardTitle>
+                            <CardText>
+                                This is my project, so good!
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </div>
+                )
+            //C#
+            case 1:
+                return (
+                    <div>
+                        <h1>This is c#</h1>
+                    </div>
+                )
+            //c++
+            case 2:
+                return (
+                    <div>
+                        <h1>This is c++</h1>
+                    </div>
+                )
+            //Java
+            case 3:
+                return (
+                    <div>
+                        <h1>This is yhava</h1>
+                    </div>
+                )
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>C#</Tab>
+                    <Tab>C++</Tab>
+                    <Tab>Java</Tab>
+                </Tabs>
+
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">
+                            {this.toggleCategories()}
+                        </div>
+                    </Cell>
+                </Grid>
+
+            </div>
         );
     }
 }
